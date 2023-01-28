8506bb3566c8481e6d23fb032bfe5a3829257682
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-28 19:03:37 +0100

    React app development

--- star-wars-temple/src/components/FilmDetails.jsx
+++ star-wars-temple/src/components/FilmDetails.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { skywalkerSagaThunk } from "../Redux/skywalkerSagaThunk";
 
 export const FilmDetails = () => {
 
+    const skywalkerSagaFilms = useSelector(state => state.skywalkerSaga.films);
+
     let params = useParams();
 
+    let dispatch = useDispatch();
+
     let filmName = params.filmname;
+
+    useEffect(() => {
+        if(!skywalkerSagaFilms.length) dispatch(skywalkerSagaThunk);
+        let neededFilm = skywalkerSagaFilms.find(elem => elem.title === filmName);
+        console.log(neededFilm)
+    })
 
     return (
         <div style={{color: 'white'}}>
