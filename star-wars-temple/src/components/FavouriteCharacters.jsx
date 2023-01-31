df134e2f00ebaf2c0c7acd4ce36cb8227f25f4ed
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-31 19:20:05 +0100

    React app development

--- star-wars-temple/src/components/FavouriteCharacters.jsx
+++ star-wars-temple/src/components/FavouriteCharacters.jsx
@@ -10,6 +10,7 @@ export const FavouriteCharacters = () => {
 
     const [favouriteCharactersInLocalStorage, setFavouriteCharactersInLocalStorage] = useState([]);
     const [currentFlex, setCurrentFlex] = useState(false);
+    const [finallyCategory, setFinallyCategory] = useState("");
 
     useEffect(() => {
         const data = localStorage.getItem("favouriteChacarters") ? JSON.parse(localStorage.getItem("favouriteChacarters")) : [];
@@ -29,6 +30,26 @@ export const FavouriteCharacters = () => {
         dispatch(removeHero());
     }
 
+    const generateFavouriteClan = () => {
+        let lengthArray = [];
+        let sith = favouriteCharactersInLocalStorage.filter(hero => hero.clan === 'Sith');
+        let bountyHunter = favouriteCharactersInLocalStorage.filter(hero => hero.clan === 'Bounty Hunter');
+        let droids = favouriteCharactersInLocalStorage.filter(hero => hero.clan === 'Droids');
+        let clansArray = [sith, bountyHunter, droids];
+        lengthArray.push(sith.length, bountyHunter.length, droids.length);
+        let maxLengthOfClan = Math.max.apply(null, lengthArray);
+        let resultClan = clansArray.find(elem => {
+            return elem.length === maxLengthOfClan;
+        });
+        let finallyCategory = "";
+        resultClan.forEach(elem => {
+            if(elem.clan === 'Sith') finallyCategory = 'Sith';
+            else if(elem.clan === 'Droids') finallyCategory = 'Droids';
+            else if(elem.clan === 'Bounty Hunter') finallyCategory = 'Bounty Hunter';
+        });
+        setFinallyCategory(finallyCategory);
+    }
+
     return (
         <div className="FavouriteCharacters"> 
             {favouriteCharactersInLocalStorage === [] || favouriteCharactersInLocalStorage === null || favouriteCharactersInLocalStorage === undefined 
@@ -44,7 +65,19 @@ export const FavouriteCharacters = () => {
                 ?
                 null
                 :
-                <button type="button" onClick={clearLocalStorageFavourite}>Clear Favourite</button>
+                <div className="ToolsFavourite">
+                    <button type="button" onClick={clearLocalStorageFavourite}>Clear Favourite</button>
+                    <br/>
+                    <br/>
+                    <button type="button" onClick={generateFavouriteClan}>Generate clan</button>
+                </div>
+            }
+            {
+                finallyCategory !== ""
+                ?
+                <div style={{color: 'white'}}>Your best category is {finallyCategory}</div>
+                : 
+                null
             }
         </div>
     )
