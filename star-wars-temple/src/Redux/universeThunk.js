bbc571fc488437031536e452b7289cab8e92b575
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-31 18:38:37 +0100

    React app development

--- star-wars-temple/src/Redux/universeThunk.js
+++ star-wars-temple/src/Redux/universeThunk.js
@@ -1,7 +1,7 @@
 import { getUniverse } from "./universeSlice";
 
 export const universeThunk = (dispatch) => {
-    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/e0ffbb361d15adcd7382e9c1df796608/raw/9b2d7efbe518b567d3982c68297f0913a091d2cc/UniverseStarWars",
+    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/e0ffbb361d15adcd7382e9c1df796608/raw/9c243f537ac66bf2e1771a517205bd84b570a486/UniverseStarWars",
         {
             method: 'get'
         }
