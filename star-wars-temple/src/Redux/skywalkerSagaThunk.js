aa2018e7ff7be21c5b76314286b85ea0755e26c3
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-02-02 15:34:56 +0100

    React app development

--- star-wars-temple/src/Redux/skywalkerSagaThunk.js
+++ star-wars-temple/src/Redux/skywalkerSagaThunk.js
@@ -7,7 +7,7 @@ export const skywalkerSagaThunk = async (dispatch) => {
         const response = await fetch("https://gist.githubusercontent.com/AliakseiMalinouski/c87236e0291af2e22c8fa91cf77b8921/raw/3bb5e6dd0e62d6f3bac9239308c5105e3e30076d/SkywalkerSagaArray");
         if(response.ok) {
             const data = await response.json();
-            dispatch(updateLoadStateSkywalkerFilms(3));
+            dispatch(updateLoadStateSkywalkerFilms(2));
             dispatch(setFilms(data));
         }
         else {
