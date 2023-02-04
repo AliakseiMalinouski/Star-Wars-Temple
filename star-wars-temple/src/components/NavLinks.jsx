5d0310dbffdec6001c801a1e1241f56c55d97f38
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-02-04 15:02:24 +0100

    React app development

--- star-wars-temple/src/components/NavLinks.jsx
+++ star-wars-temple/src/components/NavLinks.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import { starWarsEvents } from '../events';
 import { useEffect, useCallback } from 'react';
-
+import { useLocation } from 'react-router-dom';
 
 export const NavLinks = ({name, url, namePage}) => {
+
+    let location = useLocation();
+
+    let currentPage = location.pathname;
 
     const changeLocation = useCallback(() => {
         starWarsEvents.emit("changeLocation", name);
     }, [name])
 
     useEffect(() => {
-        if(name === 'DataBank') changeLocation();
-    }, [changeLocation, name])
+        if(currentPage === 'DataBank') changeLocation();
+    }, [changeLocation, currentPage])
 
     if(name === namePage) {
         return (
