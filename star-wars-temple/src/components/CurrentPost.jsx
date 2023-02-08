286395cf071020d5d501c122193cdf6eba948a28
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-02-08 17:10:24 +0100

    React app development

--- star-wars-temple/src/components/CurrentPost.jsx
+++ star-wars-temple/src/components/CurrentPost.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { categoriesNewsThunk } from "../Redux/categoriesNewsThunk";
 import { useSelector, useDispatch } from "react-redux";
 import { InformationAboutCurrentPost } from "./InformationAboutCurrentPost";
+import { NavLink } from "react-router-dom";
 
 export const CurrentPost = React.memo(() => {
 
@@ -31,9 +32,12 @@ export const CurrentPost = React.memo(() => {
     )
 
     return (
+        <>
+        <NavLink to='/news' className="ReturnToNewsPage"><img src="https://img.icons8.com/color/256/star-wars-naboo-ship.png" alt="Star Wars Naboo Ship"/><span>Back to the news filter</span></NavLink>
         <div className="CurrentPost">
             {currentPostMemoizeed}
         </div>
+        </>
     )
     
 })
